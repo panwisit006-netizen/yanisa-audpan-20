2c7d7a369fcb654f835079111130b5ebe9548323
panwisit006-netizen <panwisit006@gmail.com>
2026-09-16 11:35:08 +0700

    Update Examination_5.py

--- Examination_5.py
+++ Examination_5.py
@@ -7,4 +7,11 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-age = int(input())
+age = int(input("กรอกอายุ: "))
+
+if age < 12:
+    print("จ่าย 50")
+elif age < 60:
+    print("จ่าย 100")
+else:
+    print("เข้าฟรี (0)")

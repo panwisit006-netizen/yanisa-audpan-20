d1018cc7ac79393c2cb0f6525488661caa4d7ad1
panwisit006-netizen <panwisit006@gmail.com>
2026-09-16 11:34:46 +0700

    Update Examination_4.py

--- Examination_4.py
+++ Examination_4.py
@@ -7,4 +7,15 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-score = int(input())
+score = int(input("กรุณากรอกคะแนนสอบ: "))
+
+if score >= 80:
+    print("เกรด A")
+elif score >= 70:
+    print("เกรด B")
+elif score >= 60:
+    print("เกรด C")
+elif score >= 50:
+    print("เกรด D")
+else:
+    print("เกรด F")
